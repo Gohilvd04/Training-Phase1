b389a918ae4801b1ad04fb73d5ee5f967748bac5
Gohil Vishal <gohilvd04@gmail.com>
2023-10-09 21:43:32 +0530

    C# Learning

---
+++ C# Demo/Sealed Method/Sealed Method/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Sealed_Method
+{
+
+    class Parent
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("Parent Method");
+        }
+    }
+
+    class Child : Parent
+    {
+
+        // sealed method further cannot be override.....
+         public sealed override void show()
+        {
+            Console.WriteLine("Child Method");
+        }
+    }
+
+    class ChildGrand : Child
+    {
+
+        // we cannot override show method further because this method is sealed in Child class
+        public void show()
+        {
+            Console.WriteLine("Hello GrandChild");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ChildGrand c = new ChildGrand();
+
+            c.show();
+            
+        }
+    }
+}
