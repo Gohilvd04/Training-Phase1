5a067f23b6d2def58da7a82498b7bdd3b5dbf5e4
Gohil Vishal <gohilvd04@gmail.com>
2023-10-11 17:06:03 +0530

    C# Learning

---
+++ C# Demo/Anonymous Function/Anonymous Function/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Anonymous_Function
+{
+
+    // we can use anonymous function with delegates
+    // without delegates we cannot use anonymous function....
+
+    /// <summary>
+    /// Delegate for anonymous method
+    /// </summary>
+    /// <param name="a"></param>
+    public delegate void MyDelegate(int a);
+
+    internal class Program
+    {
+        public static void Meth(MyDelegate del, int a)
+        {
+            
+            Console.WriteLine("A in meth method before del : "+ a);
+            a += 10;
+            del(a);
+
+        }
+        static void Main(string[] args)
+        {
+            /*
+             * Method without name is called anonymous method 
+             * can be defined using delegate keyword 
+             * it is introduced in c# 2.0 version
+             * can be assign to object of delegate type
+             * no need to specify return type, we can return value using return keyword with return type of delegate
+             * we cannot use access modifiers with anonymous method
+             * cannot contain jump statements like go to, break, continue..
+             * cannot access ref or out parameter of outer method
+             * Delegates allow methods to be passed as parameters
+             * can be used as event handler
+             */
+
+
+            //MyDelegate md = delegate (int a)
+            //{
+            //    a += 10;
+            //    Console.WriteLine(a);
+            //};
+
+            //int a = 5;
+            //md(a);
+            //Console.WriteLine(a);
+
+            Meth(delegate (int a){ a += 10; Console.WriteLine(a); },5);
+        }
+    }
+}
---
+++ C# Demo/Delegates/Delegates/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Delegates
+{
+
+    /// <summary>
+    /// Delegates are reference to the method.
+    /// it is used to encapsulate the method 
+    /// there are 3 types of delegates
+    /// single cast, multi cast and multiple delegates
+    /// single cast: point to the single method
+    /// multiple: multiple delegates can be define to poin multiple methods 
+    /// multi cast: single delegate can wrap multiple methods... +=,-= are used
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+
+    public delegate void Calculation(int a, int b);
+    //public delegate void IncDcr(int a);
+    //public delegate void DelegateShow();
+
+
+    internal class Program
+    {
+        static void Addition(int a, int b)
+        {
+            Console.WriteLine("Sum : {0}", a + b);
+        }
+        
+        static void Substraction(int a, int b)
+        {
+            Console.WriteLine("Sub : {0}", a - b);
+        }static void Multiplication(int a, int b)
+        {
+            Console.WriteLine("Mul : {0}", a * b);
+        }static void Division(int a, int b)
+        {
+            Console.WriteLine("Div : {0}", a / b);
+        }
+        static void Main(string[] args)
+        {
+
+            Calculation obj = new Calculation(Program.Addition);
+
+            //Multi cast delegate using +=, -=
+
+            obj += Substraction; 
+            obj += Multiplication;
+            obj += Division;
+            obj -= Multiplication;
+
+            obj(5, 3);
+
+            //obj = Substraction;
+            //obj(5, 3);
+
+            //obj = Multiplication;
+            //obj(5, 3);
+            
+            //obj = Division;
+            //obj(5, 3);
+        }
+    }
+}
