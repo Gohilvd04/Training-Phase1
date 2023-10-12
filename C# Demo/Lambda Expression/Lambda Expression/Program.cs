fe742475ab14e2c8004bfed16cddf636169b9170
Gohil Vishal <gohilvd04@gmail.com>
2023-10-12 17:12:47 +0530

    C# Learning

---
+++ C# Demo/Lambda Expression/Lambda Expression/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lambda_Expression
+{
+
+    public delegate void MyDel(int num);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+             /*
+              * Lambda expression introduce in 3.0
+              * It simplifies anonymous function........ shorthand for anonymous function
+              * does not require to specify the data type of parameter
+             */
+
+
+
+            MyDel obj = (a) => {
+                a += 5;
+                Console.WriteLine(a);
+            };
+
+            obj(5);
+        }
+    }
+}
