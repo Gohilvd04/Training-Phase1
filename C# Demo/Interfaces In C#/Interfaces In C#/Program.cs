ae0c12d1088a66b2972660995309ff49aa8792f7
Gohil Vishal <gohilvd04@gmail.com>
2023-10-16 22:28:59 +0530

    C# Learning

---
+++ C# Demo/Interfaces In C#/Interfaces In C#/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Interfaces_In_C_
+{
+
+    /// <summary>
+    /// By Default all methods of interface are abstract and public
+    /// don't allow explicit access modifiers 
+    /// cannot have variables
+    /// </summary>
+    interface IEmployee
+    {
+        void Show();
+    }
+
+    class PartTimeEmp : IEmployee
+    {
+        public void Show()
+        {
+            Console.WriteLine("IEmployee interface");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Interface can have reference 
+            // We cannot create or instantiate the interface
+            IEmployee Emp = new PartTimeEmp();
+
+           // emp = new PartTimeEmp();
+            Emp.Show();
+        }
+    }
+}
